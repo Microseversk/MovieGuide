027df01a00e1579dd4e55f84a179b32f108e0ebf
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-04-23 23:06:22 +0700

    Setting project
    - Install dependencies
    - Add aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<>App</>)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)

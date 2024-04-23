2a06f3ae5460d72feefa525556679a992ae7c8e6
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-04-23 23:15:26 +0700

    Add Roboto font as main

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)

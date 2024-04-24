2b6eb684d620a2dd3abec3c55fcb1dda3701ffa6
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-04-24 16:41:15 +0700

    Optimize img loading

--- src/hooks/useMoviesPopular.ts
+++ src/hooks/useMoviesPopular.ts
@@ -2,11 +2,11 @@ import getMoviePopular from '@src/services/MovieService/get/getMoviePopular'
 import { useQuery } from '@tanstack/react-query'
 
 const useMoviesPopular = (page: number) => {
-	const { data, isLoading } = useQuery({
+	const { data, isLoading, error } = useQuery({
 		queryKey: ['popularMoviesFeed', page],
 		queryFn: () => getMoviePopular({ page: page ? page : 1 }),
 	})
-	return { data, isLoading }
+	return { data, isLoading, error }
 }
 
 export default useMoviesPopular
--- src/hooks/useSimilarMovies.ts
+++ src/hooks/useSimilarMovies.ts
@@ -2,7 +2,7 @@ import getSimilarMoviesById from '@src/services/MovieService/get/getSimilarMovie
 import { useQuery } from '@tanstack/react-query'
 
 const useSimilarMovies = (id: number) => {
-	const { data, isLoading } = useQuery({
+	const { data, isLoading, error } = useQuery({
 		queryKey: ['similarFilms', id],
 		queryFn: () => getSimilarMoviesById(Number(id)),
 		select: data => data.data.results,
@@ -10,6 +10,7 @@ const useSimilarMovies = (id: number) => {
 	return {
 		data,
 		isLoading,
+		error,
 	}
 }
 
